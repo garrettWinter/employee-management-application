98cd209a29e784a557fda5f1f1e413ea922e6d3b
Garrett Winter <Garrett_Winter@hotmail.com>
2023-01-20 21:05:08 -0600

    Adjusted the seed and schema of the tables. Also created departments report and started roles

--- db/reports/viewAllDepartments.sql
+++ db/reports/viewAllDepartments.sql
@@ -1,1 +1,4 @@
 USE ema_db;
+
+SELECT department_id as "Department ID", department_name as "Department Name", date_created as "Date Created", last_updated as "Updated On"
+FROM departments;
--- db/reports/viewAllRoles.sql
+++ db/reports/viewAllRoles.sql
@@ -1,1 +1,4 @@
 USE ema_db;
+
+SELECT title as "Title", role_id as "Role ID", department_id, salary AS "Salary"
+FROM roles;
