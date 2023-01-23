ce92f8dff2703671ffd31baabb934205a41b6cc0
Garrett Winter <Garrett_Winter@hotmail.com>
2023-01-22 23:24:35 -0600

    Setup route for viewUtilizedBudget

--- routes/reports.js
+++ routes/reports.js
@@ -68,14 +68,6 @@ ORDER BY departments.department_name ASC;`,req.params.department_id, (err, resul
 }
 );
 
-
-
-
-
-
-
-
-
 // GET Route for viewEmployeesByManager
 reports.get('/viewEmployeesByManager', (req, res) => {
     db.query(`SELECT
@@ -129,9 +121,29 @@ ORDER BY emp1.first_name ASC;`,req.params.manager_emp_id, (err, result) => {
 );
 
 
-// // GET Route for the notes page
-// reports.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '../public/notes.html'))
-// );
+// GET Route for viewUtilizedBudget
+reports.get('/viewUtilizedBudget', (req, res) => {
+    db.query(`SELECT 
+	departments.department_name AS 'Department',
+	departments.department_ID AS 'Department ID',
+    FORMAT (departments.department_budget,'c', 'en-US') AS 'Allocated Budget ($)',
+    FORMAT ((SUM(roles.salary)),'c', 'en-US') AS 'Utilized Budget ($)',
+    FORMAT ((departments.department_budget - SUM(roles.salary)),'c', 'en-US') AS 'Budget Variance ($)',
+    departments.date_created as "Date Created",
+    departments.last_updated as "Updated On"
+
+FROM employees as emp1
+JOIN roles ON roles.role_id = emp1.role_id
+JOIN departments ON departments.department_id = roles.department_id
+LEFT JOIN employees as emp2 ON emp1.manager_emp_id = emp2.employee_id
+GROUP BY departments.department_id;`, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        console.table(result);
+        res.json(result);
+      });
+}
+);
 
 module.exports = reports;
